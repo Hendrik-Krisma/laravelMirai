d2d5a5904edde3c119b436430ec8ddfb94d00efa
Chintya <chintya.natalie@itbss.ac.id>
2024-05-22 09:28:29 +0700

    tambah table invoices

---
+++ miraiDessertLaravel/database/migrations/2024_05_22_014625_create_tb_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer ('orderID');
+            $table->integer ('issueData');
+            $table->string ('dueData');
+            $table->integer ('paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_invoices');
+    }
+}
